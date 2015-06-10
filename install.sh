b6876a0cda8d4c6f7cfad1e32bb41486a70e9bca
Vance <vance@axxe.co.uk>
2015-06-10 17:57:13 +0100

    added installation of pandoc

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc \
-       vimrc.local vimrc.bundles.local vimrc.before.local \
-       "    # list of files/folders to symlink in homedir
+    vimrc.local vimrc.bundles.local vimrc.before.local \
+    "    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -36,9 +36,9 @@ install_zsh () {
     # Test to see if zshell is installed.  If it is:
     if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
         # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-#        if [[ ! -d $dir/oh-my-zsh/ ]]; then
-#            git clone http://github.com/robbyrussell/oh-my-zsh.git
-#        fi
+        #        if [[ ! -d $dir/oh-my-zsh/ ]]; then
+        #            git clone http://github.com/robbyrussell/oh-my-zsh.git
+        #        fi
         # Set the default shell to zsh if it isn't currently set to zsh
         if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
             chsh -s $(which zsh)
@@ -65,5 +65,23 @@ install_spf13 () {
         curl http://j.mp/spf13-vim3 -L -o - | sh
     fi
 }
+
+install_pandoc () {
+    # Test to see if pandoc is installed.  If it is:
+    if [ ! -f /usr/bin/pandoc ] && [ ! -f /usr/local/bin/pandoc ]; then
+        # If pandoc isn't installed, get the platform of the current machine
+        platform=$(uname);
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            sudo apt-get install pandoc
+            # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            echo "Please install pandoc (http://pandoc.org/), then re-run this script!"
+            exit
+        fi
+    fi
+}
+
 install_zsh
 install_spf13
+install_pandoc
