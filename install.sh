e1fd828b1ef51aa03c4dc65d92459a8395fc55bf
Vance <vance@axxe.co.uk>
2015-06-10 18:37:43 +0100

    Updated zsh settings

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc \
     vimrc.local vimrc.bundles.local vimrc.before.local \
-    zshrc \
+    zshrc oh-my-zsh \
     "    # list of files/folders to symlink in homedir
 
 ##########
