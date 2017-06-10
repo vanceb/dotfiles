4a86cbc7670953182bd5019afb05b7b9976a3743
Vance <vance@axxe.co.uk>
2017-06-10 16:37:37 +0100

    Updated fish prompt to improve git status

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,13 +14,14 @@ set gray (set_color -o black)
 
 #  Fish git prompt
 #set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_showcolorhints 'yes'
 set __fish_git_prompt_showdirtystate 'yes'
 #set __fish_git_prompt_showstashstate 'yes'
 #set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
+#set __fish_git_prompt_color_branch yellow
+#set __fish_git_prompt_color_upstream_ahead green
+#set __fish_git_prompt_color_upstream_behind red
 
 # # Status Chars
 #set __fish_git_prompt_char_dirtystate '⚡'
